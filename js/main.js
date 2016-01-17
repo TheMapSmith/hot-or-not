cd0688318c122df636f80ec06bec35328b237d17
Stephen <stephen@mapsmith.net>
2016-01-17 14:34:54 -0500

    switch all 3 names in both picks

--- js/main.js
+++ js/main.js
@@ -1,15 +1,49 @@
 // http://stackoverflow.com/questions/23148292/javascript-function-change-content-onclick
+
+var person;
+
  function changeText(pick) {
+   if (!person) {
+     alert("Who are you?")
+
+   // randomly pick 6 new items from the list
+   // http://stackoverflow.com/questions/5915096/get-random-item-from-javascript-array
+   var topFirstName = nameList[Math.floor(Math.random()*nameList.length)];
+   var topMiddleName1 = nameList[Math.floor(Math.random()*nameList.length)];
+   var topMiddleName2 = nameList[Math.floor(Math.random()*nameList.length)];
+
+   var botFirstName = nameList[Math.floor(Math.random()*nameList.length)];
+   var botMiddleName1 = nameList[Math.floor(Math.random()*nameList.length)];
+   var botMiddleName2 = nameList[Math.floor(Math.random()*nameList.length)];
+
     if(pick==1){
-        document.getElementById('topFirstName').innerHTML ='Jules';
-        document.getElementById('topMiddleName1').innerHTML ='Ludwig';
-        document.getElementById('topMiddleName2').innerHTML ='Horatio';
-        return false;
+      // add points to the list items
+
+
     }
     else {
-      document.getElementById('botFirstName').innerHTML ='Mike';
-      document.getElementById('botMiddleName1').innerHTML ='Jones';
-      document.getElementById('botMiddleName2').innerHTML ='Ebenezer';
-      return false;
+      // add points to the list items
+
+
     }
+
+    // display new items
+      document.getElementById('topFirstName').innerHTML = topFirstName;
+    //  document.getElementById('topMiddleName1').innerHTML = topMiddleName1;
+    //  document.getElementById('topMiddleName2').innerHTML = topMiddleName2;
+
+      document.getElementById('botFirstName').innerHTML = botFirstName;
+    //  document.getElementById('botMiddleName1').innerHTML = botMiddleName1;
+    //  document.getElementById('botMiddleName2').innerHTML = botMiddleName2;
+  } // end name if
  }
+
+var nameList = [
+"Phoenix","DeNiro","Beauregard","Astro","Farnsworth","Ebenezer","Cyrus","Johannes",
+"Artemis","Herman","Sandy","Dante","Apollo","Nelly","Rosecrans","Yuri","Yosemite",
+"Linford","Aeolian","Romeo","Edwin","Jefferson","Germane","Hector","Achilles",
+"Lionel","Linus","Waldon","Bjorn","Soren","Remy","Cecil","Talon","Locke","Fletcher",
+"Otnel","Naismith","Abner","Erasmus","Raphe","Percival","Marmaduke","Archibald",
+"Rockwell","Bannon","Cullen","Kellen","Valentino","Santiago","Niles","Burl","Ludwig",
+"Emlyn","Baker","Atlas","Bright"
+];
